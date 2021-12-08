bc19fdbabec622e1608710b41d8b0c4452ffcf3a
Shashi Kant <shashikantsantosh@gmail.com>
2021-12-08 13:35:30 +0000

    Script update

--- k8_copy_jars_to_pods.sh
+++ k8_copy_jars_to_pods.sh
@@ -12,28 +12,54 @@ echo "$mailbox"
 jar=$2
 echo "$jar"
 
-common_jar="zm-common-8.8.0.1629273575.jar"
-store_jar="zm-store-8.8.0.1629273575.jar"
+common_jar="zm-common-8.8.0.1637265606.jar"
+store_jar="zm-store-8.8.0.1637265606.jar"
 
 if [ "$jar" == "common" ];
 then
-	kubectl exec "$mailbox" -- touch /tmp/PASS-LIVENESS
-	m_pid=$(kubectl exec "$mailbox" -- ps -ef | grep mailboxd/etc/jetty.xml | grep -v grep  | awk '{print $1}')
-	kubectl exec -it "$mailbox" -- kill -9 "$m_pid"
-    kubectl cp "$common_jar" "$mailbox":/opt/zimbra/lib/jars/zimbracommon.jar
-    kubectl exec -it "$mailbox" -- chmod 444 /opt/zimbra/lib/jars/zimbracommon.jar
-    kubectl exec -it "$mailbox" -- chown root:root /opt/zimbra/lib/jars/zimbracommon.jar
-	kubectl exec "$mailbox" -- ./startup.sh
+    if [ "$mailbox" == "all" ];
+    then
+        for pod in $(kubectl get pods | grep mailbox | awk '{print $1}'); do
+			kubectl exec "$pod" -- touch /tmp/PASS-LIVENESS
+			m_pid=$(kubectl exec "$pod" -- ps -ef | grep mailboxd/etc/jetty.xml | grep -v grep  | awk '{print $1}')
+			kubectl exec -it "$pod" -- kill -9 "$m_pid"
+			kubectl cp "$common_jar" "$pod":/opt/zimbra/lib/jars/zimbracommon.jar
+			kubectl exec -it "$pod" -- chmod 444 /opt/zimbra/lib/jars/zimbracommon.jar
+			kubectl exec -it "$pod" -- chown root:root /opt/zimbra/lib/jars/zimbracommon.jar
+			kubectl exec "$pod" -- ./startup.sh
+        done
+    else
+		kubectl exec "$mailbox" -- touch /tmp/PASS-LIVENESS
+		m_pid=$(kubectl exec "$mailbox" -- ps -ef | grep mailboxd/etc/jetty.xml | grep -v grep  | awk '{print $1}')
+		kubectl exec -it "$mailbox" -- kill -9 "$m_pid"
+		kubectl cp "$common_jar" "$mailbox":/opt/zimbra/lib/jars/zimbracommon.jar
+		kubectl exec -it "$mailbox" -- chmod 444 /opt/zimbra/lib/jars/zimbracommon.jar
+		kubectl exec -it "$mailbox" -- chown root:root /opt/zimbra/lib/jars/zimbracommon.jar
+		kubectl exec "$mailbox" -- ./startup.sh
+    fi
 
 elif [ "$jar" == "store" ];
 then
-	kubectl exec "$mailbox" -- touch /tmp/PASS-LIVENESS
-	m_pid=$(kubectl exec "$mailbox" -- ps -ef | grep mailboxd/etc/jetty.xml | grep -v grep  | awk '{print $1}')
-	kubectl exec -it "$mailbox" -- kill -9 "$m_pid"
-    kubectl cp "$store_jar" "$mailbox":/opt/zimbra/lib/jars/zimbrastore.jar
-    kubectl exec -it "$mailbox" -- chmod 444 /opt/zimbra/lib/jars/zimbrastore.jar
-    kubectl exec -it "$mailbox" -- chown root:root /opt/zimbra/lib/jars/zimbrastore.jar
-	kubectl exec "$mailbox" -- ./startup.sh
+    if [ "$mailbox" == "all" ];
+    then
+        for pod in $(kubectl get pods | grep mailbox | awk '{print $1}'); do
+            kubectl exec "$pod" -- touch /tmp/PASS-LIVENESS
+            m_pid=$(kubectl exec "$pod" -- ps -ef | grep mailboxd/etc/jetty.xml | grep -v grep  | awk '{print $1}')
+            kubectl exec -it "$pod" -- kill -9 "$m_pid"
+            kubectl cp "$store_jar" "$pod":/opt/zimbra/lib/jars/zimbrastore.jar
+            kubectl exec -it "$pod" -- chmod 444 /opt/zimbra/lib/jars/zimbrastore.jar
+            kubectl exec -it "$pod" -- chown root:root /opt/zimbra/lib/jars/zimbrastore.jar
+            kubectl exec "$pod" -- ./startup.sh
+        done
+    else
+	    kubectl exec "$mailbox" -- touch /tmp/PASS-LIVENESS
+	    m_pid=$(kubectl exec "$mailbox" -- ps -ef | grep mailboxd/etc/jetty.xml | grep -v grep  | awk '{print $1}')
+	    kubectl exec -it "$mailbox" -- kill -9 "$m_pid"
+        kubectl cp "$store_jar" "$mailbox":/opt/zimbra/lib/jars/zimbrastore.jar
+        kubectl exec -it "$mailbox" -- chmod 444 /opt/zimbra/lib/jars/zimbrastore.jar
+        kubectl exec -it "$mailbox" -- chown root:root /opt/zimbra/lib/jars/zimbrastore.jar
+	    kubectl exec "$mailbox" -- ./startup.sh
+    fi
 
 elif [ "$jar" == "all" ];
 then
